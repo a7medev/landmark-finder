bb16cefbd338e15137b05aa466eb45d95e497e06
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-09-21 11:50:06 +0200

    chore: configure navigation theme

--- App/navigation/AppNavigator.js
+++ App/navigation/AppNavigator.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from '../screens/HomeScreen';
+import useColors from '../theme/Colors';
 
 const Stack = createNativeStackNavigator();
 
 const options = { headerShown: false };
 
 const AppNavigator = () => {
+  const colors = useColors();
+
+  const theme = useMemo(
+    () => ({
+      dark: false,
+      colors: {
+        primary: colors.primary,
+        background: colors.white,
+        card: colors.white,
+        text: colors.black,
+        border: colors.light,
+        notification: colors.primary
+      }
+    }),
+    [colors]
+  );
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={options} />
       </Stack.Navigator>
